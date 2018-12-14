ad26e02aa5971edc4ea19c70ca6898555422c527
alexpud <alexandre.maciele@gmail.com>
2018-12-14 19:19:02 -0200

    Updated secret handshake solution

--- secret-handshake/secret_handshake.exs
+++ secret-handshake/secret_handshake.exs
@@ -16,49 +16,49 @@ defmodule SecretHandshake do
   values = [10000, 1000, 100, 10, 1]
   use Bitwise
   @spec commands(code :: integer) :: list(String.t())
-  def commands(code) do 
+  def commands(code) do
     []
     |> wink?(code)
-    |> doubleWink?(code)
-    |> closeEyes?(code)
+    |> double_wink?(code)
+    |> close_eyes?(code)
     |> jump?(code)
     |> reverse?(code)
   end
 
-  def wink?(list, value) do
+  defp wink?(list, value) do
     case band(value, 1) do
       1 -> List.insert_at(list,0, "wink")
       _ -> list
     end
   end
 
-  def doubleWink?(list, value) do
+  defp double_wink?(list, value) do
     case band(value, 2) do
       2 -> List.insert_at(list,0, "double blink")
       _ -> list
     end
   end
 
-  def closeEyes?(list, value) do
+  defp close_eyes?(list, value) do
     case band(value, 4) do
       4 -> List.insert_at(list,0, "close your eyes")
       _ -> list
     end
   end
 
-  
-  def jump?(list, value) do
+
+  defp jump?(list, value) do
     case band(value, 8) do
       8 -> List.insert_at(list,0, "jump")
       _    -> list
     end
   end
 
-  def reverse?(list, value) do
+  defp reverse?(list, value) do
     case band(value, 16) do
       16 -> list
       _ -> Enum.reverse(list)
     end
   end
-  
+
 end
