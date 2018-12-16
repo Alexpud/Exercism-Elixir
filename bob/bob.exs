8e4a111fecced55b7432d567210e36f2448422fc
alexpud <alexandre.maciele@gmail.com>
2018-12-16 09:50:02 -0200

    Fixes suggested by mentor

--- bob/bob.exs
+++ bob/bob.exs
@@ -1,19 +1,22 @@
 defmodule Bob do
   def hey(input) do
     cond do
-      yelling_question?(input) -> "Calm down, I know what I'm doing!"
-      question?(input) && !yelling?(input)-> "Sure."
-      yelling?(input) -> "Whoa, chill out!"
       String.trim(input) == "" -> "Fine. Be that way!"
+      yelling_question?(input) -> "Calm down, I know what I'm doing!"
+      question?(input) -> "Sure."
+      yelling?(input) -> "Whoa, chill out!"
       true -> "Whatever."
     end
   end
 
-  defp yelling?(input) do
-    String.trim(input) != "" &&
+  defp yelling_question?(input) do
+    question?(input) &&
     talking_forcefully?(input) &&
-    !String.ends_with?(input, "?") &&
-    not (has_numbers?(input) && !has_letters?(input))
+    has_letters?(input)
+  end
+
+  defp question?(input) do
+    String.ends_with?(input, "?")
   end
 
   defp talking_forcefully?(input) do
@@ -24,18 +27,15 @@ defmodule Bob do
     Regex.match?(~r/[A-Za-z]/, input)
   end
 
+  defp yelling?(input) do
+    String.trim(input) != "" &&
+    talking_forcefully?(input) &&
+    !String.ends_with?(input, "?") &&
+    not (has_numbers?(input) && !has_letters?(input))
+  end
+
   defp has_numbers?(input) do
     Regex.match?(~r/[1-9]/, input)
-  end
-
-  defp question?(input) do
-    String.ends_with?(input, "?")
-  end
-
-  defp yelling_question?(input) do
-    question?(input) &&
-    talking_forcefully?(input) &&
-    has_letters?(input)
   end
 
 end
