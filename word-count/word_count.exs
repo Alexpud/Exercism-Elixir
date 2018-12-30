3f51f86894bc049ce995d2ee5bdba6f491adacd2
alexpud <alexandre.maciele@gmail.com>
2018-12-30 20:58:31 -0200

    Part of word count

--- word-count/word_count.exs
+++ word-count/word_count.exs
@@ -9,23 +9,17 @@ defmodule Words do
     String.normalize(sentence, :nfd)
     |> remove_punctuation
     |> String.downcase
-    |> split
+    |> split_string_separators
     |> group_words_array_by_word
     |> count_grouped_words_ocurrence
     |> Map.new
-    # words_array = String.split(sentence)
-    # grouped_array = Enum.group_by(words_array, fn x -> x end)
-    # tuple_array = for {word, ocurrences} <- grouped_array do
-    #   {word, Enum.count(ocurrences)}
-    # end
-    # Map.new(tuple_array)
   end
 
   defp remove_punctuation(phrase) do
     String.replace(phrase, ~r/(?!-|_)[[:punct:]]*[:]*/, "")
   end
 
-  defp split(phrase) do
+  defp split_string_separators(phrase) do
     String.split(phrase, ~r/[ |_]/, trim: true)
   end
 
@@ -38,5 +32,4 @@ defmodule Words do
       {word, Enum.count(ocurrences)}
     end
   end
-
 end
