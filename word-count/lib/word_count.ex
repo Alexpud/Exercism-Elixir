12fac3a7cb2867d73abb21c13868192db6ad88c3
alexpud <alexandre.maciele@gmail.com>
2020-01-18 11:29:23 -0300

    Word count solution

--- word-count/lib/word_count.ex
+++ word-count/lib/word_count.ex
@@ -6,36 +6,18 @@ defmodule WordCount do
   """
   @spec count(String.t()) :: map
   def count(sentence) do
-  	Regex.scan(~r/\b(\w*[\w-]+)\b/u, sentence)
-  	|> remove_duplicates
+  	Regex.scan(~r/[^_\s,:!&@$%^&]+/u, sentence)
   	|> List.flatten
-#    String.normalize(sentence, :nfd)
-#    remove_punctuation(sentence)
     |> downcase_elements
-#    |> split_by_separators
     |> group_words_array_by_word
     |> count_grouped_words_ocurrence
     |> Map.new
-  end
-
-  defp remove_duplicates(sentence) do
-  	for x <- sentence do
-  		Enum.uniq(x)
-  	end
   end
 
   defp downcase_elements(elements) do
   	for element <- elements do
   		String.downcase(element)
   	end
-  end
-
-  defp remove_punctuation(phrase) do
-    String.replace(phrase, ~r/(?!-|_)[[:punct:]]*[:]*/, "")
-  end
-
-  defp split_by_separators(phrase) do
-    String.split(phrase, ~r/[ |_]/, trim: true)
   end
 
   defp group_words_array_by_word(words_array) do
